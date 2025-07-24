50ebc26a686a9a447ca072af8b4479d549562399
Josh Olson <Olson.dev@Gmail.com>
2025-07-24 10:49:54 -0500

    Miscellaneous cleanup (#1)
    * Miscellaneous improvements: typo fixed, rename `db` to `connection` so it's easier to talk about, alignment in SQL for quicker scanning, more data to make it easier to see sorting is implemented correctly, make it so the default namespace isn't some weird thing like `sm_backend_coding_challenge, reminders about commands that are available in the files pertinent to them.
    
    * Add recommended VS Code extensions - C# Dev Kit & Docker

--- seed.sql
+++ seed.sql
@@ -1,10 +1,16 @@
+-- Run this command to reseed the database:
+-- make db-seed
 USE FruitsDB;
 GO
 
-TRUNCATE TABLE Fruits;
-
-INSERT INTO Fruits (Name, Color) VALUES ('Apple', 'Red');
-INSERT INTO Fruits (Name, Color) VALUES ('Banana', 'Yellow');
-INSERT INTO Fruits (Name, Color) VALUES ('Cherry', 'Red');
-INSERT INTO Fruits (Name, Color) VALUES ('Grape', 'Purple');
-INSERT INTO Fruits (Name, Color) VALUES ('Strawberry', 'Red');
+TRUNCATE TABLE dbo.Fruits;
+INSERT dbo.Fruits (Name, Color) VALUES ('Apple'     , 'Red');
+INSERT dbo.Fruits (Name, Color) VALUES ('Banana'    , 'Yellow');
+INSERT dbo.Fruits (Name, Color) VALUES ('Lime'      , 'Green');
+INSERT dbo.Fruits (Name, Color) VALUES ('Cherry'    , 'Red');
+INSERT dbo.Fruits (Name, Color) VALUES ('Apple'     , 'Green');
+INSERT dbo.Fruits (Name, Color) VALUES ('Grape'     , 'Purple');
+INSERT dbo.Fruits (Name, Color) VALUES ('Lemon'     , 'Yellow');
+INSERT dbo.Fruits (Name, Color) VALUES ('Grape'     , 'Green');
+INSERT dbo.Fruits (Name, Color) VALUES ('Strawberry', 'Red');
+INSERT dbo.Fruits (Name, Color) VALUES ('Kiwi'      , 'Green');
